900a9d7f8b6b9cf2ebde2b6c922b06d7cf1cfb7c
José Vielma <josevglod@gmail.com>
2024-05-22 16:14:27 -0400

    add animations and toast provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authorizationInterceptor } from './interpetors/authorization.interceptor';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authorizationInterceptor])),
+    provideToastr(),
+    provideAnimations()
   ],
 };
