009551dbdd9ac4ce366aa0e88a7af39fdad2e4ae
José Vielma <josevglod@gmail.com>
2024-05-22 16:15:29 -0400

    remove innecesary values from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'GreenTracker';
-  BASE_URL = environment.BASE_URL;
-  constructor(){
-    console.log(this.BASE_URL)
-  }
+  
 }
