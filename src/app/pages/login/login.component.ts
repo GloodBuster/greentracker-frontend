f319cef9fc118b6483af44ec0ea467433c1980f2
José Vielma <josevglod@gmail.com>
2024-05-23 14:58:28 -0400

    update login with primeng

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,17 +7,31 @@ import {
 } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { PanelModule } from 'primeng/panel';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    InputTextModule,
+    FloatLabelModule,
+    PanelModule,
+    PasswordModule,
+    ButtonModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  authService = inject(AuthService)
-  toastService = inject(ToastrService)
+  authService = inject(AuthService);
+  toastService = inject(ToastrService);
+
+  loading = false;
 
   userLoginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -25,13 +39,10 @@ export class LoginComponent {
   });
 
   submitForm() {
+    this.loading = true;
     console.log(this.userLoginForm.value);
     this.toastService.success('Login successful');
+    this.loading = false;
   }
 
-  visiblePassword = false;
-
-  togglePasswordVisibility() {
-    this.visiblePassword = !this.visiblePassword;
-  }
 }
