11eb39b3ea4e4b1b71f815e1b9c9e01f83815707
José Vielma <josevglod@gmail.com>
2024-05-28 17:56:20 -0400

    remove form to another component and add ucab image

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,48 +1,12 @@
-import { Component, inject } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { AuthService } from '../../services/auth/auth.service';
-import { ToastrService } from 'ngx-toastr';
-import { InputTextModule } from 'primeng/inputtext';
-import { FloatLabelModule } from 'primeng/floatlabel';
-import { PanelModule } from 'primeng/panel';
-import { PasswordModule } from 'primeng/password';
-import { ButtonModule } from 'primeng/button';
+import { Component } from '@angular/core';
+import { LoginFormComponent } from '../../components/login/login-form/login-form.component';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    InputTextModule,
-    FloatLabelModule,
-    PanelModule,
-    PasswordModule,
-    ButtonModule,
-  ],
+  imports: [LoginFormComponent, ImageModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-  authService = inject(AuthService);
-  toastService = inject(ToastrService);
-
-  loading = false;
-
-  userLoginForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required]),
-  });
-
-  submitForm() {
-    this.loading = true;
-    console.log(this.userLoginForm.value);
-    this.toastService.success('Login successful');
-    this.loading = false;
-  }
-
-}
+export class LoginComponent {}
