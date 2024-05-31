b0ae81869941274207e4eda602ff6bf0589665d6
José Vielma <josevglod@gmail.com>
2024-05-30 20:22:31 -0400

    create response and error interface

---
+++ src/app/interfaces/responses/error.ts
@@ -0,0 +1,11 @@
+import { HttpErrorResponse } from "@angular/common/http";
+
+export interface ErrorResponse {
+  message: string;
+  error: string;
+  statusCode: number;
+}
+
+export interface CustomHttpErrorResponse extends Omit<HttpErrorResponse, 'error'> {
+    error: ErrorResponse;
+  }
