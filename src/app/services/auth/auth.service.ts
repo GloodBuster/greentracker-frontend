93dd8bd537b4ac45cf73f3b0ed33faaf54866604
José Vielma <josevglod@gmail.com>
2024-05-30 20:25:09 -0400

    implements get me and login services

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { User } from '../../interfaces/user';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { LoginForm, LoginResponse } from '../../interfaces/login/login';
+import { map, Observable } from 'rxjs';
+import { Response } from '../../interfaces/responses/response';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  readonly http = inject(HttpClient);
+  private readonly BASE_URL = environment.BASE_URL;
 
-  constructor() { }
+  getMe(): Observable<Response<User>> {
+    return this.http.get<Response<User>>(`${this.BASE_URL}/users/me`);
+  }
+
+  login(loginForm: LoginForm): Observable<Response<LoginResponse>> {
+    return this.http.post<Response<LoginResponse>>(
+      `${this.BASE_URL}/auth/login`,
+      loginForm
+    );
+  }
 }
