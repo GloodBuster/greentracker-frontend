469fe8209edfd82a4529bd112427ea488a706480
José Vielma <josevglod@gmail.com>
2024-06-01 10:48:41 -0400

    add paginated response

--- src/app/interfaces/responses/response.ts
+++ src/app/interfaces/responses/response.ts
@@ -2,3 +2,14 @@ export interface Response<T> {
   statusCode: number;
   data: T;
 }
+
+export interface PaginatedResponse<T> {
+  statusCode: number;
+  data: {
+    items: T[];
+    pageIndex: number;
+    itemsPerPage: number;
+    pageCount: number;
+    itemCount: number;
+  };
+}
