771a1354b246c2533c8a5d476c1991266297d894
Cristina Morales Padron <cristimoralespadron@icloud.com>
2024-06-02 20:20:58 -0400

    Acople edit indicators

--- src/app/components/indicador/dialog-edit/dialog-edit.component.ts
+++ src/app/components/indicador/dialog-edit/dialog-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, inject } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
@@ -8,6 +8,9 @@ import { ButtonModule } from 'primeng/button';
 import { PanelModule } from 'primeng/panel';
 import { ToastrService } from 'ngx-toastr';
 import { CommonModule } from '@angular/common';
+import { IndicatorService } from '../../../services/indicator/indicator.service';
+import { Indicator } from '../../../interfaces/indicator/indicator';
+import { ErrorResponse } from '../../../interfaces/responses/error';
 
 @Component({
   selector: 'app-dialog-edit',
@@ -17,39 +20,63 @@ import { CommonModule } from '@angular/common';
   styleUrl: './dialog-edit.component.scss'
 })
 export class DialogEditComponent {
-  @Input() indicador: any;
+  @Input() indicador: Indicator = {index: 0, englishName: '', spanishAlias: ''};
   @Input() visible: boolean = false;
   @Output() hide: EventEmitter<any> = new EventEmitter();
-  @Output() update: EventEmitter<any> = new EventEmitter<any>();
+  @Output() update: EventEmitter<{value: Indicator, index: number}> = new EventEmitter<any>();
   @Output() delete: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor(private indicatorService: IndicatorService) {
+
+   }
 
   showConfirmDialog = false;
 
   indicadorForm = new FormGroup({
-    id: new FormControl(0),
-    name: new FormControl(''),
-    SpanishAlias: new FormControl(''),
+    index: new FormControl(this.indicador.index, [Validators.required]),
+    englishName: new FormControl(this.indicador.englishName, [Validators.required]),
+    spanishAlias: new FormControl(this.indicador.spanishAlias, [Validators.required]),
   });
 
   toastService = inject(ToastrService);
 
   hideDialogEdit() {
+    this.indicadorForm.reset();
+    this.indicador = {index: 0, englishName: '', spanishAlias: ''};
     this.hide.emit();
   }
+  
   ngOnChanges(changes: SimpleChanges) {
     if (changes['indicador'] && changes['indicador'].currentValue) {
       this.indicadorForm.setValue({
-        id: changes['indicador'].currentValue.id,
-        name: changes['indicador'].currentValue.name,
-        SpanishAlias: changes['indicador'].currentValue.SpanishAlias,
+        index: changes['indicador'].currentValue.index,
+        englishName: changes['indicador'].currentValue.englishName,
+        spanishAlias: changes['indicador'].currentValue.spanishAlias,
       });
     }
   }
+
   submitForm() {
     if (this.indicadorForm.valid) {
-      this.update.emit(this.indicadorForm.value);
-      this.hideDialogEdit();
-      this.toastService.success('Indicador editado con éxito');
+      const indicator: Indicator = {
+        index: this.indicadorForm.value.index ?? 0, 
+        englishName: this.indicadorForm.value.englishName ?? '', 
+        spanishAlias: this.indicadorForm.value.spanishAlias ?? '', 
+      };
+    
+      this.indicatorService.editIndicator(indicator).subscribe(
+        {
+          next:(response) => {
+          this.update.emit({value: indicator, index: this.indicador.index});
+          this.hideDialogEdit();
+          this.toastService.success('Indicador editado con éxito');
+          this.indicadorForm.reset(); 
+        },
+        error: (error: ErrorResponse) => {
+          this.toastService.error(error.message);
+          console.log('Error al editar el indicador:', error.message);
+        }}
+      );
     } else {
       this.toastService.error('Ha ocurrido un error');
       console.log('Formulario no válido');
@@ -66,10 +93,19 @@ export class DialogEditComponent {
   }
 
   confirmDelete() {
-    this.delete.emit(this.indicadorForm.value);
-    this.hideDialogEdit();
-    this.toastService.success('Indicador eliminado con éxito');
-    this.showConfirmDialog = false;
+    const index = this.indicador.index;
+      this.indicatorService.deleteIndicator(index).subscribe({
+        next: (response) => {
+          this.delete.emit(index); 
+          this.hideDialogEdit();
+          this.toastService.success('Indicador eliminado con éxito');
+          this.showConfirmDialog = false;
+        },
+        error: (error: ErrorResponse) => {
+          this.toastService.error(error.message);
+          console.log('Error al eliminar el indicador:', error.message);
+        }}
+      );
   }
 
   cancelDelete() {
