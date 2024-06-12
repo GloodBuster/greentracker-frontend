97ef2fa2643804db6323866ae0e8ab989c22a197
José Vielma <josevglod@gmail.com>
2024-06-12 18:31:54 -0400

    add unit details interface

--- src/app/interfaces/units/units.ts
+++ src/app/interfaces/units/units.ts
@@ -1,3 +1,4 @@
+import { Categories } from "../categories/categories";
 
 export interface Units {
     name: string;
@@ -21,3 +22,9 @@ export interface Units {
     password: '',
   };
   
+  export interface UnitDetails {
+    id: string;
+    name: string;
+    email: string;
+    recommendedCategories: Categories[];
+  }
