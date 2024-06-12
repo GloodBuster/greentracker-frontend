96f936e398b5fe509f9d89a5c89db76503ebe207
José Vielma <josevglod@gmail.com>
2024-06-12 16:30:32 -0400

    add get all indicators

--- src/app/components/admin/dashboard/evidence-matrix/evidence-matrix.component.ts
+++ src/app/components/admin/dashboard/evidence-matrix/evidence-matrix.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { TableModule } from 'primeng/table';
+import { IndicatorService } from '../../../../services/indicator/indicator.service';
+import { IndicatorDetails } from '../../../../interfaces/indicator/indicator';
+import { CustomHttpErrorResponse } from '../../../../interfaces/responses/error';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-evidence-matrix',
   standalone: true,
   imports: [TableModule],
   templateUrl: './evidence-matrix.component.html',
-  styleUrl: './evidence-matrix.component.scss'
+  styleUrl: './evidence-matrix.component.scss',
 })
 export class EvidenceMatrixComponent {
+  indicators: IndicatorDetails[] = [];
 
+  constructor(
+    private readonly indicatorService: IndicatorService,
+    private readonly toastService: ToastrService
+  ) {
+    this.indicatorService.getAllIndicators().subscribe({
+      next: (response) => {
+        this.indicators = response.data;
+      },
+      error: (error: CustomHttpErrorResponse) => {
+        if (error.error.statusCode === 500) {
+          this.toastService.error('Ha ocurrido un error inesperado');
+        } else {
+          this.toastService.error(error.error.message);
+        }
+      },
+    });
+  }
 }
