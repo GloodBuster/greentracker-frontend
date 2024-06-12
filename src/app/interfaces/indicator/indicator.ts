382aa4fd9ddeb4192c6aa2148506b335f70687aa
José Vielma <josevglod@gmail.com>
2024-06-12 16:30:08 -0400

    add indicator details interface

--- src/app/interfaces/indicator/indicator.ts
+++ src/app/interfaces/indicator/indicator.ts
@@ -1,5 +1,11 @@
+import { CategorieDetails } from "../categories/categories";
+
 export interface Indicator {
     index: number;
     englishName: string;
     spanishAlias: string;
+}
+
+export interface IndicatorDetails extends Indicator {
+    categories: CategorieDetails[];
 }
