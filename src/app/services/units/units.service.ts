7727cafac7ce6f0962db37a3081c114076e568c4
José Vielma <josevglod@gmail.com>
2024-06-19 19:30:38 -0400

    add get all inits service

--- src/app/services/units/units.service.ts
+++ src/app/services/units/units.service.ts
@@ -6,49 +6,42 @@ import {
   Response,
 } from '../../interfaces/responses/response';
 import { Observable } from 'rxjs';
-import { UnitsForm, UnitsGet } from '../../interfaces/units/units';
+import { UnitDetails, UnitsForm, UnitsGet } from '../../interfaces/units/units';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
-export class UnitsService{
-    readonly http = inject(HttpClient);
-    private readonly BASE_URL = environment.BASE_URL;
+export class UnitsService {
+  readonly http = inject(HttpClient);
+  private readonly BASE_URL = environment.BASE_URL;
 
-    getUnits(
-        page: number = 1,
-        itemsPerPage: number = 10
-    ): Observable<PaginatedResponse<UnitsGet>> {
-        return this.http.get<PaginatedResponse<UnitsGet>>(
-            `${this.BASE_URL}/units?pageIndex=${page}&itemsPerPage=${itemsPerPage}`
-        );
-    }
+  getAllUnits(): Observable<Response<UnitDetails[]>> {
+    return this.http.get<Response<UnitDetails[]>>(`${this.BASE_URL}/units`);
+  }
 
-    addNewUnit(
-        unit: UnitsForm
-    ): Observable<Response<UnitsForm>> {
-        return this.http.post<Response<UnitsForm>>(
-            `${this.BASE_URL}/units`,
-            unit
-        );
-    }
+  getUnits(
+    page: number = 1,
+    itemsPerPage: number = 10
+  ): Observable<PaginatedResponse<UnitsGet>> {
+    return this.http.get<PaginatedResponse<UnitsGet>>(
+      `${this.BASE_URL}/units?pageIndex=${page}&itemsPerPage=${itemsPerPage}`
+    );
+  }
 
-    updateUnit(
-        id: string,
-        unit: UnitsForm
-    ): Observable<Response<UnitsForm>> {
-        return this.http.put<Response<UnitsForm>>(
-            `${this.BASE_URL}/units/${id}`,
-            unit
-        );
-    }
+  addNewUnit(unit: UnitsForm): Observable<Response<UnitsForm>> {
+    return this.http.post<Response<UnitsForm>>(`${this.BASE_URL}/units`, unit);
+  }
 
-    deleteUnit(
-        id: string
-    ): Observable<Response<UnitsForm>> {
-        return this.http.delete<Response<UnitsForm>>(
-            `${this.BASE_URL}/units/${id}`
-        );
-    }
+  updateUnit(id: string, unit: UnitsForm): Observable<Response<UnitsForm>> {
+    return this.http.put<Response<UnitsForm>>(
+      `${this.BASE_URL}/units/${id}`,
+      unit
+    );
+  }
 
+  deleteUnit(id: string): Observable<Response<UnitsForm>> {
+    return this.http.delete<Response<UnitsForm>>(
+      `${this.BASE_URL}/units/${id}`
+    );
+  }
 }
