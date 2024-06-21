f2501170a3f45a8a7466a24b6fd24b50de5a3479
Cristina Morales Padron <cristimoralespadron@icloud.com>
2024-06-20 23:38:07 -0400

    Create unit recommended categories fixed

--- src/app/components/unidad/dialog-create/dialog-create.component.ts
+++ src/app/components/unidad/dialog-create/dialog-create.component.ts
@@ -13,74 +13,92 @@ import { PanelModule } from 'primeng/panel';
 import { ToastrService } from 'ngx-toastr';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { UnitsService } from '../../../services/units/units.service';
-import { UnitsForm } from '../../../interfaces/units/units';
+import { CategoriesData, CategoriesForm, Indicators, UnitsForm } from '../../../interfaces/units/units';
 import { CustomHttpErrorResponse } from '../../../interfaces/responses/error';
 
 @Component({
   selector: 'app-dialog-create',
   standalone: true,
-  imports: [DialogModule, InputTextModule, FormsModule, FloatLabelModule, ButtonModule, PanelModule, ReactiveFormsModule, MultiSelectModule],
+  imports: [
+    DialogModule,
+    InputTextModule,
+    FormsModule,
+    FloatLabelModule,
+    ButtonModule,
+    PanelModule,
+    ReactiveFormsModule,
+    MultiSelectModule,
+  ],
   templateUrl: './dialog-create.component.html',
-  styleUrl: './dialog-create.component.scss'
+  styleUrl: './dialog-create.component.scss',
 })
 export class DialogCreateComponent {
-@Input() visible: boolean = false;
-@Output() hide: EventEmitter<any> = new EventEmitter();
-@Output() create: EventEmitter<any> = new EventEmitter<any>();
-loading = false;
+  @Input() visible: boolean = false;
+  @Output() hide: EventEmitter<any> = new EventEmitter();
+  @Output() create: EventEmitter<any> = new EventEmitter<any>();
+  loading = false;
+  indicators: Indicators[] = [];
+  selectedCategories: CategoriesData[] = [];
+  selectedIndicator: Indicators | undefined;
+  indicatorIndex = 0;
 
-constructor(private unitsService: UnitsService) { }
-
-hideDialog() {
-  this.hide.emit();
-  this.unitsForm.reset();
-}
-toastService = inject(ToastrService);
-unitsForm = new FormGroup({
-  name: new FormControl('', [Validators.required]),
-  email: new FormControl('', [Validators.required, Validators.email]),
-  password: new FormControl('', [Validators.required]),
-});
-
-submitForm() {
-  if(this.unitsForm.valid){
-    this.loading = true;
-    const unit = this.unitsForm.value as UnitsForm;
-    this.unitsService
-    .addNewUnit(unit)
-    .subscribe({
+  constructor(private unitsService: UnitsService) {
+    this.unitsService.getAllIndicators().subscribe({
       next: (response) => {
-        this.create.emit(response.data);
-        this.toastService.success('Unidad agregada con éxito');
-        this.loading = false;
-        this.hideDialog();
+        this.indicators = response.data;
       },
       error: (error: CustomHttpErrorResponse) => {
-        this.toastService.error('Error al agregar la unidad');
-        console.error('Error al agregar la unidad:', error);
-      }
+        console.error('Error al obtener los indicadores:', error);
+      },
     });
   }
-}
 
-/*category: any[] | undefined;
+  hideDialog() {
+    this.hide.emit();
+    this.unitsForm.reset();
+  }
+  toastService = inject(ToastrService);
+  unitsForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    recommendedCategories: new FormControl<CategoriesData[] | CategoriesForm[]>(
+      [],
+      [Validators.required]
+    ),
+  });
 
-    selectedCategory: string | undefined;
-    categorias: any[] = [];
-    ngOnInit() {
-      this.category = categoriasData;
+  submitForm() {
+    if (this.unitsForm.valid) {
+      this.loading = true;
+      const categories = this.unitsForm.value
+        .recommendedCategories as CategoriesData[];
+      this.unitsForm.value.recommendedCategories = categories?.map(
+        (category) => {
+          const indicatorIndex = this.indicators.find((indicator) =>
+            indicator.categories.includes(category)
+          );
+          return {
+            categoryName: category.name,
+            indicatorIndex: indicatorIndex?.index ?? 0,
+          };
+        }
+      );
+      const unit = this.unitsForm.value as UnitsForm;
+      console.log('unit:', unit);
+      this.unitsService.addNewUnit(unit).subscribe({
+        next: (response) => {
+          this.create.emit(response.data);
+          this.toastService.success('Unidad agregada con éxito');
+          this.loading = false;
+          this.hideDialog();
+        },
+        error: (error: CustomHttpErrorResponse) => {
+          this.toastService.error('Error al agregar la unidad');
+          console.error('Error al agregar la unidad:', error);
+        },
+      });
     }
-
-
-
-
-unitForm = new FormGroup({
-  id: new FormControl(9),
-  nombre: new FormControl('', [Validators.required]),
-  correo: new FormControl('', [Validators.required, Validators.email]),
-  categorias: new FormControl([])
-});*/
-
-
+  }
 }
 
