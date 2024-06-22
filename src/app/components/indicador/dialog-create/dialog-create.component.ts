505a1dc6060cc8a30af2d99e8f7dfa688ad0f874
Cristina Morales Padron <cristimoralespadron@icloud.com>
2024-06-21 23:37:50 -0400

    create Indicators fixed

--- src/app/components/indicador/dialog-create/dialog-create.component.ts
+++ src/app/components/indicador/dialog-create/dialog-create.component.ts
@@ -14,11 +14,12 @@ import { ToastrService } from 'ngx-toastr';
 import { IndicatorService } from '../../../services/indicator/indicator.service';
 import { Indicator } from '../../../interfaces/indicator/indicator';
 import { ErrorResponse } from '../../../interfaces/responses/error';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
   selector: 'app-dialog-create',
   standalone: true,
-  imports: [DialogModule, InputTextModule, FormsModule, FloatLabelModule, ButtonModule, PanelModule, ReactiveFormsModule],
+  imports: [DialogModule, InputTextModule, FormsModule, FloatLabelModule, ButtonModule, PanelModule, ReactiveFormsModule, InputNumberModule],
   templateUrl: './dialog-create.component.html',
   styleUrl: './dialog-create.component.scss'
 })
@@ -26,6 +27,7 @@ export class DialogCreateComponent {
   @Input() visible: boolean = false;
   @Output() hide: EventEmitter<any> = new EventEmitter();
   @Output() create: EventEmitter<any> = new EventEmitter<any>();
+  loading = false;
 
   constructor(private indicatorService: IndicatorService) { }
 
@@ -41,6 +43,7 @@ export class DialogCreateComponent {
 
   submitForm() {
     if (this.indicatorForm.valid) {
+      this.loading = true;
       const indicator: Indicator = {
         index: this.indicatorForm.value.index ?? 0, 
         englishName: this.indicatorForm.value.englishName ?? '', 
@@ -53,13 +56,16 @@ export class DialogCreateComponent {
           this.hideDialog();
           this.toastService.success('Indicador creado con éxito');
           this.indicatorForm.reset();
+          this.loading = false;
         },
         (error: ErrorResponse) => {
           this.toastService.error(error.message);
+          this.loading = false;
         }
       );
     } else {
       this.toastService.error('Ha ocurrido un error');
+      this.loading = false;
     }
   }
 }
