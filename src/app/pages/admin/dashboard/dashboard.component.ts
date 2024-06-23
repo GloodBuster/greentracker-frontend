47081f33e5f8d7422dc90b6a6ecd04ccb4475474
José Vielma <josevglod@gmail.com>
2024-06-22 23:35:56 -0400

    show charge period

--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -2,28 +2,57 @@ import { Component } from '@angular/core';
 import { ProgressBarModule } from 'primeng/progressbar';
 import { EvidenceMatrixComponent } from '../../../components/admin/dashboard/evidence-matrix/evidence-matrix.component';
 import { SkeletonModule } from 'primeng/skeleton';
+import { ChargePeriodService } from '../../../services/charge-period/charge-period.service';
+import { CustomHttpErrorResponse } from '../../../interfaces/responses/error';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { convertDateFormat } from '../../../utils/date';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ProgressBarModule, EvidenceMatrixComponent, SkeletonModule],
+  imports: [
+    ProgressBarModule,
+    EvidenceMatrixComponent,
+    SkeletonModule,
+    ToastrModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
   remainingDays = 0;
-  startPeriodDate = new Date('06-01-2024').toLocaleDateString().slice(0, 10);
-  endPeriodDate = new Date('06-10-2024').toLocaleDateString().slice(0, 10);
-  unitsWithEvidencePercentage = 80;
-  chargedEvidence = 100;
+  startPeriodDate = '';
+  endPeriodDate = '';
+  unitsWithEvidencePercentage = 0;
+  chargedEvidence = 0;
   loading = true;
 
-  constructor() {
-    const today = new Date();
-    this.remainingDays = Math.floor(
-      (new Date(this.endPeriodDate).getTime() - today.getTime()) /
-        (1000 * 60 * 60 * 24)
-    );
+  constructor(
+    private readonly chargePeriodService: ChargePeriodService,
+    private readonly toastService: ToastrService
+  ) {
+    this.chargePeriodService.getChargePeriod().subscribe({
+      next: (response) => {
+        this.startPeriodDate = convertDateFormat(
+          response.data.startTimestamp.slice(0, 10)
+        );
+
+        this.endPeriodDate = convertDateFormat(
+          response.data.endTimestamp.slice(0, 10)
+        );
+        const start = new Date(response.data.startTimestamp);
+        const end = new Date(response.data.endTimestamp);
+        const diff = Math.abs(end.getTime() - start.getTime());
+        this.remainingDays = Math.ceil(diff / (1000 * 60 * 60 * 24));
+      },
+      error: (error: CustomHttpErrorResponse) => {
+        if (error.error.statusCode === 500) {
+          this.toastService.error('Ha ocurrido un error inesperado');
+        } else {
+          this.toastService.error(error.error.message);
+        }
+      },
+    });
   }
 
   editChargedEvidence(value: number) {
