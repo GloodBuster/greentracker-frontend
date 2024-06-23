1f2cce33aafd0bc1b0f4168b5d73234f80881bba
José Vielma <josevglod@gmail.com>
2024-06-22 23:33:43 -0400

    add charge period interfaces

---
+++ src/app/interfaces/charge-period/chargePeriod.ts
@@ -0,0 +1,10 @@
+export interface ChargePeriod {
+  id: string;
+  startTimestamp: string;
+  endTimestamp: string;
+}
+
+export interface ChargePeriodForm {
+  startTimestamp: string;
+  endTimestamp: string;
+}
