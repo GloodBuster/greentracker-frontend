e8629cba666d0388c55d0b876dc7f87ea2c3b8cb
José Vielma <josevglod@gmail.com>
2024-06-23 00:15:09 -0400

    add more dates functions

--- src/app/utils/date.ts
+++ src/app/utils/date.ts
@@ -1,4 +1,17 @@
-export function convertDateFormat(dateString: string): string {
+export const convertDateFormat = (dateString: string): string => {
   const [year, month, day] = dateString.split('-');
   return `${day}/${month}/${year}`;
-}
+};
+
+export const adjustDateForTimezone = (date: Date) => {
+  const offset = date.getTimezoneOffset();
+  if (offset > 0) {
+    date.setDate(date.getDate() + 1);
+  }
+  return date;
+};
+
+export const convertToUTCDate = (timestamp: string): Date => {
+  const [year, month, day] = timestamp.split('T')[0].split('-').map(Number);
+  return new Date(Date.UTC(year, month - 1, day));
+};
