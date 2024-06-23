ee496c4d7462072966f704cc5941b6f62dc786cf
José Vielma <josevglod@gmail.com>
2024-06-23 11:45:13 -0400

    add notification overlay to navbar

--- src/app/components/admin/navbar/navbar.component.ts
+++ src/app/components/admin/navbar/navbar.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { ImageModule } from 'primeng/image';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { NotificationCardComponent } from '../notification-card/notification-card.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ImageModule, ButtonModule],
+  imports: [
+    ImageModule,
+    ButtonModule,
+    OverlayPanelModule,
+    NotificationCardComponent,
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
