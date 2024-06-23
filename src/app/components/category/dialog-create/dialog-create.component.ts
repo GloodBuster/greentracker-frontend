6c047287bb5687ac028411938928ecf519e56ba3
Cristina Morales Padron <cristimoralespadron@icloud.com>
2024-06-22 20:47:02 -0400

    Create category

--- src/app/components/category/dialog-create/dialog-create.component.ts
+++ src/app/components/category/dialog-create/dialog-create.component.ts
@@ -1,5 +1,17 @@
-import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  inject,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { DropdownModule } from 'primeng/dropdown';
@@ -10,35 +22,45 @@ import { CategoriesService } from '../../../services/categories/categories.servi
 import { ToastrService } from 'ngx-toastr';
 import { ActivatedRoute } from '@angular/router';
 import { CustomHttpErrorResponse } from '../../../interfaces/responses/error';
+import { Criterion } from '../../../interfaces/criteria/criteria';
+import { MultiSelectModule } from 'primeng/multiselect';
 
 @Component({
   selector: 'app-dialog-create',
   standalone: true,
-  imports: [DialogModule,
+  imports: [
+    DialogModule,
     ButtonModule,
     ReactiveFormsModule,
     FloatLabelModule,
     InputTextModule,
-    DropdownModule,],
+    DropdownModule,
+    MultiSelectModule
+  ],
   templateUrl: './dialog-create.component.html',
-  styleUrl: './dialog-create.component.scss'
+  styleUrl: './dialog-create.component.scss',
 })
-export class DialogCreateComponent {
+export class DialogCreateComponent implements OnInit {
   @Input() visible: boolean = false;
   @Output() hideDialog: EventEmitter<void> = new EventEmitter();
-  @Output() createCategory: EventEmitter<CategoriesForm> = new EventEmitter();
+  @Output() createCategory: EventEmitter<any> = new EventEmitter();
+  @Input() criteria: Criterion[] = [];
   categoryService = inject(CategoriesService);
   toastService = inject(ToastrService);
   indicatorIndex = 0;
   loading = false;
 
-  constructor(private readonly route: ActivatedRoute) {
-    const indicatorIndex = this.route.snapshot.paramMap.get('indicatorIndex');
-    if (indicatorIndex) this.indicatorIndex = parseInt(indicatorIndex);
+  constructor(private readonly route: ActivatedRoute) {}
+  ngOnInit() {
+    this.route.queryParamMap.subscribe((params) => {
+      const indicatorIndex = params.get('index');
+      if (indicatorIndex) this.indicatorIndex = parseInt(indicatorIndex);
+    });
   }
 
   categoryForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
+    criteria: new FormControl<Criterion[]>([], []),
   });
 
   hide() {
@@ -47,24 +69,23 @@ export class DialogCreateComponent {
   }
 
   submitForm() {
-    if(this.categoryForm.valid){
+    if (this.categoryForm.valid) {
       this.loading = true;
       const category = this.categoryForm.value as CategoriesForm;
       this.categoryService
-      .createCategory(this.indicatorIndex, category)
-      .subscribe({
-        next: (response) => {
-          this.createCategory.emit(response.data);
-          this.toastService.success('Categoria agregada con éxito');
-          this.loading = false;
-          this.hide();
-        },
-        error: (error: CustomHttpErrorResponse) => {
-          this.toastService.error('Ha ocurrido un error inesperado');
-          this.loading = false;
-        },
-      });
+        .createCategory(this.indicatorIndex, category)
+        .subscribe({
+          next: (response) => {
+            this.createCategory.emit(response.data);
+            this.toastService.success('Categoria agregada con éxito');
+            this.loading = false;
+            this.hide();
+          },
+          error: (error: CustomHttpErrorResponse) => {
+            this.toastService.error('Ha ocurrido un error inesperado');
+            this.loading = false;
+          },
+        });
     }
   }
-
 }
