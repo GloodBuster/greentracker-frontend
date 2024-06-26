80d44c3a417107597ac6396cfc26169e6fa16d99
José Vielma <josevglod@gmail.com>
2024-06-25 20:57:26 -0400

    create activity details component

---
+++ src/app/pages/unit/activity-details/activity-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './activity-details.component.html',
+  styleUrl: './activity-details.component.scss'
+})
+export class ActivityDetailsComponent {
+
+}
