fee392e0cab97d3ca29f4f846d3fee0d6bbe2033
José Vielma <josevglod@gmail.com>
2024-06-29 13:26:56 -0400

    add dialog and animation to link evidence component

--- src/app/components/unit/link-evidence-form/link-evidence-form.component.ts
+++ src/app/components/unit/link-evidence-form/link-evidence-form.component.ts
@@ -4,6 +4,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { TagModule } from 'primeng/tag';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputTextareaModule } from 'primeng/inputtextarea';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'link-evidence-form',
@@ -13,15 +16,38 @@ import { InputTextareaModule } from 'primeng/inputtextarea';
     TagModule,
     InputTextModule,
     InputTextareaModule,
+    DialogModule,
+    ButtonModule,
   ],
   templateUrl: './link-evidence-form.component.html',
   styleUrl: './link-evidence-form.component.scss',
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateY(+100%)' }),
+        animate('800ms ease-in-out', style({ transform: 'translateY(0%)' })),
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateY(0%)' }),
+        animate('800ms ease-in-out', style({ transform: 'translateY(-100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class LinkEvidenceFormComponent {
   @Input() linkForm: LinkEvidence = getLinkEvidenceForm();
   @Output() removeEvidence: EventEmitter<void> = new EventEmitter<void>();
+  visibleDelete = false;
 
   remove() {
     this.removeEvidence.emit();
+  }
+
+  showDelete() {
+    this.visibleDelete = true;
+  }
+
+  hideDelete() {
+    this.visibleDelete = false;
   }
 }
