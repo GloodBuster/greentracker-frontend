365556ff993df4553a261bc8dbcfe87c039f0fe9
Cristina Morales Padron <cristimoralespadron@icloud.com>
2024-06-28 23:36:01 -0400

    document card

---
+++ src/app/components/activities/feedbackCards/document-evidence/document-evidence.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { DividerModule } from 'primeng/divider';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { TagModule } from 'primeng/tag';
+
+@Component({
+  selector: 'app-document-evidence',
+  standalone: true,
+  imports: [DividerModule, TagModule, InputGroupModule, InputGroupAddonModule, InputTextModule ],
+  templateUrl: './document-evidence.component.html',
+  styleUrl: './document-evidence.component.scss'
+})
+export class DocumentEvidenceComponent {
+
+}
