e419ff2af5e737ac6bfe3493a9ca843306be391b
José Vielma <josevglod@gmail.com>
2024-06-29 13:25:44 -0400

    finish document evidence component

--- src/app/components/unit/document-evidence-form/document-evidence-form.component.ts
+++ src/app/components/unit/document-evidence-form/document-evidence-form.component.ts
@@ -3,19 +3,73 @@ import {
   DocumentEvidence,
   getDocumentEvidenceForm,
 } from '../../../utils/formsTypes';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TagModule } from 'primeng/tag';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FileUploadModule } from 'primeng/fileupload';
+import { formatFileSize } from '../../../utils/files';
+import { UploadEvent } from '../image-evidence-form/image-evidence-form.component';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'document-evidence-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    TagModule,
+    InputTextareaModule,
+    FileUploadModule,
+    ButtonModule,
+    DialogModule,
+  ],
   templateUrl: './document-evidence-form.component.html',
   styleUrl: './document-evidence-form.component.scss',
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateY(+100%)' }),
+        animate('800ms ease-in-out', style({ transform: 'translateY(0%)' })),
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateY(0%)' }),
+        animate('800ms ease-in-out', style({ transform: 'translateY(-100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class DocumentEvidenceFormComponent {
   @Input() documentForm: DocumentEvidence = getDocumentEvidenceForm();
   @Output() removeEvidence: EventEmitter<void> = new EventEmitter<void>();
+  visibleDelete = false;
+  file: File | null = null;
 
   remove() {
     this.removeEvidence.emit();
   }
+
+  onUpload(event: UploadEvent) {
+    if (event.files && event.files[0]) {
+      this.file = event.files[0];
+    }
+  }
+
+  removeFile() {
+    this.file = null;
+  }
+
+  choose(event: Event, callback: any) {
+    callback();
+  }
+
+  showDelete() {
+    this.visibleDelete = true;
+  }
+
+  hideDelete() {
+    this.visibleDelete = false;
+  }
+
+  formatFileSize = formatFileSize;
 }
