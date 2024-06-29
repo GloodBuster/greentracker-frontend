c96512a6644a5abaa2fe41dd679c06ef0f100363
José Vielma <josevglod@gmail.com>
2024-06-28 23:51:44 -0400

    add evidence array and add functions

--- src/app/pages/unit/new-activity/new-activity.component.ts
+++ src/app/pages/unit/new-activity/new-activity.component.ts
@@ -1,16 +1,51 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
+import {
+  DocumentEvidence,
+  getDocumentEvidenceForm,
+  getImageEvidenceForm,
+  getLinkEvidenceForm,
+  ImageEvidence,
+  LinkEvidence,
+} from '../../../utils/formsTypes';
+import { LinkEvidenceFormComponent } from '../../../components/unit/link-evidence-form/link-evidence-form.component';
+import { ImageEvidenceFormComponent } from '../../../components/unit/image-evidence-form/image-evidence-form.component';
+import { DocumentEvidenceFormComponent } from '../../../components/unit/document-evidence-form/document-evidence-form.component';
 
 @Component({
   selector: 'app-new-activity',
   standalone: true,
-  imports: [ButtonModule, OverlayPanelModule],
+  imports: [
+    ButtonModule,
+    OverlayPanelModule,
+    ReactiveFormsModule,
+    LinkEvidenceFormComponent,
+    ImageEvidenceFormComponent,
+    DocumentEvidenceFormComponent,
+  ],
   templateUrl: './new-activity.component.html',
   styleUrl: './new-activity.component.scss',
 })
 export class NewActivityComponent {
-  print() {
-    console.log('Funcionando');
+  evidences: (ImageEvidence | LinkEvidence | DocumentEvidence)[] = [
+    getImageEvidenceForm(),
+  ];
+
+  addImageEvidence() {
+    this.evidences.push(getImageEvidenceForm());
+  }
+
+  addLinkEvidence() {
+    this.evidences.push(getLinkEvidenceForm());
+  }
+
+  addDocumentEvidence() {
+    this.evidences.push(getDocumentEvidenceForm());
+  }
+
+  removeEvidence(index: number) {
+    this.evidences.splice(index, 1);
   }
 }
