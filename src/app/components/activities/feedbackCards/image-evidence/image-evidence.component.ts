fbbad1a73d5f3024dedd94e81eaefbfbca0a288c
Cristina Morales Padron <cristimoralespadron@icloud.com>
2024-07-09 11:27:54 -0400

    Image evidence fixed
    It shows the image uploaded, and the size of the image

--- src/app/components/activities/feedbackCards/image-evidence/image-evidence.component.ts
+++ src/app/components/activities/feedbackCards/image-evidence/image-evidence.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input, inject } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { DividerModule } from 'primeng/divider';
+import { ImageModule } from 'primeng/image';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
 import { TagModule } from 'primeng/tag';
 import { TooltipModule } from 'primeng/tooltip';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-image-evidence',
   standalone: true,
-  imports: [DividerModule, TagModule, InputGroupModule, InputGroupAddonModule, InputTextModule, TooltipModule ],
+  imports: [DividerModule, TagModule, InputGroupModule, InputGroupAddonModule, InputTextModule, TooltipModule,ImageModule, CommonModule ],
   templateUrl: './image-evidence.component.html',
   styleUrl: './image-evidence.component.scss'
 })
@@ -18,7 +21,20 @@ export class ImageEvidenceComponent {
   @Input() evidence: any;
   toastService = inject(ToastrService);
   selectedFeedback = '';
+  imageSize: number = 0;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const fullUrl = 'http://149.50.140.48' + this.evidence.link;
+    this.getImageSize(fullUrl);
+  }
+
+  getImageSize(url: string) {
+    this.http.head(url, { observe: 'response' }).subscribe(response => {
+      this.imageSize = Number(response.headers.get('Content-Length'));
+    });
+  }
   async copyValue(inputValue: HTMLInputElement) {
     try {
       await navigator.clipboard.writeText(inputValue.value);
