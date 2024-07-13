ab82b7091b28ac64f29f949210febbc54bfcd144
José Vielma <josevglod@gmail.com>
2024-07-13 14:36:56 -0400

    add evidence with feedback type

--- src/app/interfaces/evidences/evidences.ts
+++ src/app/interfaces/evidences/evidences.ts
@@ -21,3 +21,18 @@ export interface DocumentEvidenceI {
   description: string;
   uploadTimestamp: string;
 }
+
+export interface EvidenceWithFeedback {
+  evidenceNumber: number;
+  link: string;
+  description: string;
+  uploadTimestamp: string;
+  type: string;
+  linkToRelatedResource?: string;
+  feedbacks: Feedback[];
+}
+
+export interface Feedback {
+  adminId: string;
+  feedback: 'approved' | 'contact_admin' | 'broken_link' | 'broken_file';
+}
